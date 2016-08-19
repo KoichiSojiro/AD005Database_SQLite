0c76c92fb7c54c8d7220f932be6ebb417351cc9d
Koichi <koichisojiro@live.com>
2016-08-19 15:23:11 +0700

    raw code Tutorials Point
    input raw code and UI from Tutorials Point without any checking

--- app/src/main/java/com/example/trannh08/ad005database_sqlite/DisplayContact.java
+++ app/src/main/java/com/example/trannh08/ad005database_sqlite/DisplayContact.java
@@ -1,13 +1,83 @@
 package com.example.trannh08.ad005database_sqlite;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.trannh08.ad005database_sqlite.classes.DBHelper;
 
 public class DisplayContact extends AppCompatActivity {
+
+    int from_Where_I_Am_Coming = 0;
+    private DBHelper mydb ;
+
+    TextView name ;
+    TextView phone;
+    TextView email;
+    TextView street;
+    TextView place;
+    int id_To_Update = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_contact);
+
+        name = (TextView) findViewById(R.id.editText_name);
+        phone = (TextView) findViewById(R.id.editText_phone);
+        email = (TextView) findViewById(R.id.editText_street);
+        street = (TextView) findViewById(R.id.editText_email);
+        place = (TextView) findViewById(R.id.editText_city);
+
+        mydb = new DBHelper(this);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras !=null)
+        {
+            int Value = extras.getInt("id");
+
+            if(Value>0){
+                //means this is the view part not the add contact part.
+                Cursor rs = mydb.getData(Value);
+                id_To_Update = Value;
+                rs.moveToFirst();
+
+                String nam = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_NAME));
+                String phon = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_PHONE));
+                String emai = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_EMAIL));
+                String stree = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_STREET));
+                String plac = rs.getString(rs.getColumnIndex(DBHelper.CONTACTS_COLUMN_CITY));
+
+                if (!rs.isClosed())
+                {
+                    rs.close();
+                }
+                Button b = (Button)findViewById(R.id.button_save);
+                b.setVisibility(View.INVISIBLE);
+
+                name.setText((CharSequence)nam);
+                name.setFocusable(false);
+                name.setClickable(false);
+
+                phone.setText((CharSequence)phon);
+                phone.setFocusable(false);
+                phone.setClickable(false);
+
+                email.setText((CharSequence)emai);
+                email.setFocusable(false);
+                email.setClickable(false);
+
+                street.setText((CharSequence)stree);
+                street.setFocusable(false);
+                street.setClickable(false);
+
+                place.setText((CharSequence)plac);
+                place.setFocusable(false);
+                place.setClickable(false);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/trannh08/ad005database_sqlite/MainActivity.java
+++ app/src/main/java/com/example/trannh08/ad005database_sqlite/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.trannh08.ad005database_sqlite;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -27,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayList contacts = mydb.getAllCotacts();
         ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1, contacts);
 
-        obj = (ListView)findViewById(R.id.listView1);
+        obj = (ListView)findViewById(R.id.listView);
         obj.setAdapter(arrayAdapter);
         obj.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
@@ -46,5 +49,36 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+
+        switch(item.getItemId())
+        {
+            case R.id.mainMenu:Bundle dataBundle = new Bundle();
+                dataBundle.putInt("id", 0);
+
+                Intent intent = new Intent(getApplicationContext(),DisplayContact.class);
+                intent.putExtras(dataBundle);
+
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public boolean onKeyDown(int keycode, KeyEvent event) {
+        if (keycode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(true);
+        }
+        return super.onKeyDown(keycode, event);
+    }
 }
